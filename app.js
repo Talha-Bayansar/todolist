c4f61198ba368edaca2cb663c26970c1560d983e
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-08-19 16:36:58 +0200

    you can now add notes

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 //selectors
-let inputNote = document.querySelector("input");
-let buttonAddNote = document.querySelector("button");
-let listNotes = document.querySelector(".todo-list");
+let notesList = document.querySelector(".todo-list");
+let addNoteButton = document.querySelector(".add-note-button");
+let inputNote = document.querySelector(".input-note");
 //functions
-let addNote = () => {
-    let pNote = document.createElement("p");
-    pNote.innerText = inputNote.value;
-    let li = document.createElement("il");
-    li.appendChild(pNote);
-    listNotes.appendChild(li);
+let addNote = (e) => {
+    //prevent form from submitting!!!
+    event.preventDefault();
+    //prevent form from submitting!!!
+    let li = document.createElement("li");
+    li.innerText = inputNote.value;
+    li.classList.add("note");
+    notesList.appendChild(li);
 }
 //eventlisteners
-buttonAddNote.addEventListener("click", addNote);
+addNoteButton.addEventListener("click", addNote);
